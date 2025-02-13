7664c5ecb582cbe50afb0fcd0fa2a7c0fe89dc18
Ruan6400 <133367215+Ruan6400@users.noreply.github.com>
2025-02-13 19:22:46 -0300

    Add files via upload

--- js/dblivro.js
+++ js/dblivro.js
@@ -1,6 +1,7 @@
 function Run(){
     let btn_showHide=document.querySelector('body>button:first-of-type')
-
+    let Dados = localStorage.getItem("livros")
+    if(Dados == null){Dados = []}
     btn_showHide.addEventListener('click',()=>{
         document.getElementById('Cadastrar').click()
         btn_showHide.innerHTML = btn_showHide.innerHTML=="Cadastrar"?"Fechar":"Cadastrar";
@@ -9,16 +10,32 @@ function Run(){
     let cadastro = document.querySelector("form")
     cadastro.addEventListener('submit',(e)=>{
         e.preventDefault()
-        alert("Achou que ia enviar informações do formulário? ACHOU ERRADO")
-        cadastro.reset()
         let informacoes = document.querySelectorAll('form input[type="text"]')
         for(let i=0;i<informacoes.length;i++){
             if(informacoes[i].value==""){
                 console.log("informações insuficientes");
                 i=informacoes.length;
+                informacoes.forEach(input=>{
+                    input.value=""
+                })
             }
         }
-        console.log("ok")
+        
+        if(informacoes[3].value==""){
+            informacoes[3].style.backgroundColor="#f55"
+        }else if(
+            informacoes[3].value.length != 5 ||
+            /[^A-Z]/.test(informacoes[3].value[0]) ||
+            /[^a-z]/.test(informacoes[3].value[4]) ||
+            /[^0-9]/.test(informacoes[3].value.slice(2,4))
+        ){
+            alert("o imbecil digitou errado")
+        }else{
+            const Livro = {titulo,autor,editora,codigo}
+            if(Dados.length == 0){
+                console.log("não tem nada aqui")
+            }
+        }
     })
 }
 
