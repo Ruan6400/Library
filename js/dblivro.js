9cd2941716be55ac0c695dbb67aec53ecaea978e
Ruan6400 <133367215+Ruan6400@users.noreply.github.com>
2025-02-13 21:11:22 -0300

    Add files via upload

--- js/dblivro.js
+++ js/dblivro.js
@@ -1,12 +1,22 @@
+let Dados;
+
 function Run(){
     let btn_showHide=document.querySelector('body>button:first-of-type')
-    let Dados = localStorage.getItem("livros")
-    if(Dados == null){Dados = []}
+    Dados = localStorage.getItem("Dados")
+    if(Dados == null){Dados = []}else{
+        Dados = JSON.parse(Dados).vetor
+    }
+
+
+
+
+    //Mostrar, ocultar menu de cadastro de livro
     btn_showHide.addEventListener('click',()=>{
         document.getElementById('Cadastrar').click()
         btn_showHide.innerHTML = btn_showHide.innerHTML=="Cadastrar"?"Fechar":"Cadastrar";
     })
 
+    //Cadastrar os livros
     let cadastro = document.querySelector("form")
     cadastro.addEventListener('submit',(e)=>{
         e.preventDefault()
@@ -29,14 +39,39 @@ function Run(){
             /[^a-z]/.test(informacoes[3].value[4]) ||
             /[^0-9]/.test(informacoes[3].value.slice(2,4))
         ){
-            alert("o imbecil digitou errado")
+            //console.log("o imbecil digitou errado")
+            cadastro.reset()
         }else{
-            const Livro = {titulo,autor,editora,codigo}
-            if(Dados.length == 0){
-                console.log("não tem nada aqui")
-            }
+            Armazenar()
+            cadastro.reset()
         }
     })
+
+    //Pesquisar no banco de dados
+    document.querySelector('body>button:last-of-type').addEventListener('click',()=>{
+        let val_busca = document.querySelector("form+input").value         
+        let consulta = Dados.filter(livro=>{
+            let atributos = Object.keys(livro)
+            for(let i=0;i<atributos.length;i++){
+                if(livro[atributos[i]].toLowerCase().includes(val_busca.toLowerCase())&&val_busca!=""){
+                    return true;
+                }
+            }
+            return false;
+        })
+        console.log(consulta)
+    })
+}
+
+
+function Armazenar(){
+    const titulo = document.getElementById('titulo').value
+    const autor = document.getElementById('autor').value
+    const editora = document.getElementById('editora').value
+    const codigo = document.getElementById('codigo').value
+    const Livro = {titulo:titulo,autor:autor,editora:editora,codigo:codigo}
+    Dados.push(Livro)
+    localStorage.setItem("Dados",JSON.stringify({vetor:Dados}));
 }
 
 document.addEventListener("DOMContentLoaded",Run)
